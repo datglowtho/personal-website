732d52582ba0c9cce8b11e274b0a52133c13cd46
Harry Uglow <hu115@ic.ac.uk>
2017-07-27 17:49:27 +0100

    Working on sidenav for Projects page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { ROUTING } from "./app.routing";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         AboutComponent,
-        HomeComponent
+        HomeComponent,
+        ProjectsComponent
     ],
     imports: [
         BrowserAnimationsModule,
