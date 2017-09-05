0a2b2dbebd0a0d49b0ef7a4803083e9584ce3ef5
Harry Uglow <hu115@ic.ac.uk>
2017-09-05 23:20:53 +0100

    Added missing variable declaration in app.component.ts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,19 @@ import { Router } from '@angular/router';
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    host: {
+        '(window:resize)': 'onResize($event)'
+    }
 })
 export class AppComponent {
+    useMobileStyle: boolean;
+
     constructor(private router: Router) {
+        this.useMobileStyle = (window.innerWidth < 768);
+    }
+
+    onResize(event) {
+        this.useMobileStyle = (event.target.innerWidth < 768);
     }
 }
